365aa448b0125ae8859d75ee5236a9e9813be60b
Steephen Resurreccion <steephen_resurreccion@dlsl.edu.ph>
2024-08-24 08:40:27 +0800

    first commit

---
+++ gitdemo.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+    using namespace std;
+
+
+    int main(){
+
+        cout<<"Hello world";
+        cout<<"Second Change";
+        cout<<"Third Change";
+
+        return 0;
+    }
+
